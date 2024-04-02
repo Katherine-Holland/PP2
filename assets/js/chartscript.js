ebde88f6683807f83eea75dc6eaf002e6f9e1b27
Katherine Weston <119933259+Katherine-Holland@users.noreply.github.com>
2024-04-02 20:00:14 +0000

    google chart tidy up
    Moved the google script out of the header and put at the bottom of the body to load scripts in the correct order. Created another .js file (chartscript) to accommodate the script function and set the call back.

---
+++ assets/js/chartscript.js
@@ -0,0 +1,24 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Lettuce', 3],
+        ['Lupin', 1],
+        ['Helebore', 1],
+        ['Herbs', 1],
+        ['Delphinium', 2]
+    ]);
+
+    var options = {
+        'title': 'Plants Snails Ate In One Night',
+        'width': 456,
+        'height': 342
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
